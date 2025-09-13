8b4e0a1598812a2c967555319f6fc7d14076ad7a
Stefan Flosbach <stefanflosbach@t-online.de>
2025-09-13 16:21:09 +0200

    sticky box und Grunddesign

---
+++ scripts/db.js
@@ -0,0 +1,27 @@
+let myDishes = [
+    {
+        "name": ,
+        "price": ,
+        "description":
+    }
+    {
+        "name": ,
+        "price": ,
+        "description":
+    }
+    {
+        "name": ,
+        "price": ,
+        "description":
+    }
+    {
+        "name": ,
+        "price": ,
+        "description":
+    }
+    {
+        "name": ,
+        "price": ,
+        "description":
+    }
+]
